ddd672fba58ae7177e186cd97823548189f62620
root <root@kolla-sm.lxd>
2016-11-07 01:19:40 +0000

    Added few script and playbook for interface configuration.

---
+++ scripts/generate_ansible_hosts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import csv
+import sys
+
+input = str(sys.argv[1])
+
+with open(input) as csvfile:
+    reader = csv.DictReader(csvfile)
+    reader.fieldnames = "hostname", "mac", "ip", "netmask", "gateway", "nameserver", "port", "seed", "ironic"
+    output = ""
+    for row in reader:
+        output += row['hostname'] + " ansible_ssh_host=" + row['ip'] + " ansible_ssh_host_ironic=" + row['ironic'] + "\n"
+
+print output
---
+++ scripts/geninput.sh
@@ -0,0 +1,23 @@
+COUNT=58
+for i in $(cat ilo.csv)
+do
+    NAME=`echo $i | cut -d',' -f1`
+    IP=`echo $i | cut -d',' -f2`
+    TYPE=`echo $i | cut -d',' -f3`
+
+    case "$TYPE" in
+      cinder)
+            SEED='ubuntu-14.04.3-server-unattended-osic-cinder'
+            ;;
+        swift)
+            SEED='ubuntu-14.04.3-server-unattended-osic-swift'
+            ;;
+        *)
+        SEED='ubuntu-14.04.3-server-unattended-osic-generic'
+            ;;
+    esac
+    MAC=`sshpass -p cobbler ssh -o StrictHostKeyChecking=no root@$IP ifconfig -a p2p1 | awk 'BEGIN{ FS="HWaddr "}{print$2}' | tr -d " \t\n\r"`
+    #hostname,mac-address,host-ip,host-netmask,host-gateway,dns,pxe-interface,cobbler-profile,ironic-ipv4
+    echo "$NAME,${MAC//[$'\t\r\n ']},172.22.0.$COUNT,255.255.252.0,172.22.0.1,8.8.8.8,p2p1,$SEED,$IP" | tee -a input.csv
+    (( COUNT++ ))
+done
