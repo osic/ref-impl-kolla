620eca832a4c10133745cb2298a7aae564fc2e6f
SHASHANK TAVILDAR <shashank.tavildar@gmail.com>
2016-11-09 16:57:16 -0600

    Update geninput.sh

--- scripts/geninput.sh
+++ scripts/geninput.sh
@@ -16,7 +16,7 @@ do
         SEED='ubuntu-14.04.3-server-unattended-osic-generic'
             ;;
     esac
-    MAC=`sshpass -p cobbler ssh -o StrictHostKeyChecking=no root@$IP ifconfig -a p2p1 | awk 'BEGIN{ FS="HWaddr "}{print$2}' | tr -d " \t\n\r"`
+    MAC=`sshpass -p cobbler ssh -o StrictHostKeyChecking=no root@$IP ifconfig -a bon0 | awk 'BEGIN{ FS="HWaddr "}{print$2}' | tr -d " \t\n\r"`
     #hostname,mac-address,host-ip,host-netmask,host-gateway,dns,pxe-interface,cobbler-profile,ironic-ipv4
     echo "$NAME,${MAC//[$'\t\r\n ']},172.22.0.$COUNT,255.255.252.0,172.22.0.1,8.8.8.8,p2p1,$SEED,$IP" | tee -a input.csv
     (( COUNT++ ))
