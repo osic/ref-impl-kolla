38cf069ec6058f5783b9f6673bd5d3a97efdcef4
SHASHANK TAVILDAR <shashank.tavildar@gmail.com>
2016-11-15 16:41:48 -0600

    Update kolla-cinder-prep.sh

--- scripts/kolla-cinder-prep.sh
+++ scripts/kolla-cinder-prep.sh
@@ -1,5 +1,5 @@
 mknod /dev/loop9 b 7 2
 dd if=/dev/zero of=/var/lib/cinder_data.img bs=1G count=20
-losetup /dev/loop2 /var/lib/cinder_data.img
+losetup /dev/loop9 /var/lib/cinder_data.img
 pvcreate /dev/loop9
 vgcreate cinder-volumes /dev/loop9
