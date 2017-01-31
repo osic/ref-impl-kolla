c5abaec888b53f4b825a09dc845c18f8f8105af5
SHASHANK TAVILDAR <shashank.tavildar@gmail.com>
2017-01-31 09:43:12 -0600

    Update re-address.sh

--- scripts/re-address.sh
+++ scripts/re-address.sh
@@ -6,6 +6,7 @@ for i in `cat hosts | awk /ansible_ssh_host/ | cut -d'=' -f2 | cut -d ' ' -f1`
 do
   /usr/bin/ssh-keyscan $1 >> ~/.ssh/known_hosts
   sshpass -p cobbler ssh root@$1 ip addr add $i/22 dev bond0
+  sshpass -p cobbler ssh root@$1 route add default gw 10.3.72.1
   printf "%s %s\n" "$i" "$1"
   shift
 done
