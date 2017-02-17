1cf0ce729702eb042a6808729853f7a16028d727
SHASHANK TAVILDAR <shashank.tavildar@gmail.com>
2017-02-16 19:00:08 -0600

    Update validate-globals.py

--- scripts/validate-globals.py
+++ scripts/validate-globals.py
@@ -9,8 +9,10 @@ if "ubuntu" in stream['kolla_base_distro'] and "source" in stream['kolla_install
 if "bond0.200" in stream["network_interface"] and "bond1" in stream["neutron_external_interface"]:
    print "Interface verified"
 else:
-   os.system("sudo sed -i \'s/^#network_interface.*/network_interface: \"bond0.200\"/g\'"+ file)
-   os.system("sudo sed -i \'s/^neutron_external_interface.*/neutron_external_interface: \"bond1\"/g\'"+ file)
+   comm_1 = "sed -i 's/^network_interface.*/network_interface: \"bond0.200\"/g' "+ file
+   comm_2 = "sed -i 's/^neutron_external_interface.*/neutron_external_interface: \"bond1\"/g' "+ file
+   os.system(comm_1)
+   os.system(comm_2)
 if "yes" in stream['enable_cinder'] and "yes" in stream['enable_heat'] and "yes" in stream['enable_horizon']:
    print "Service configuration verified"
 
